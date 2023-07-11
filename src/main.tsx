e639576a3b6017bdac66d305d79a78ab4c59ba43
danilkk <ezzfvkoko@gmail.com>
2023-07-11 23:56:40 +0300

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './1app/App.tsx'
 import './1app/styles/index.css'
 import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
         <ChakraProvider>
             <App/>
-        </ChakraProvider>
-    </React.StrictMode>,
+        </ChakraProvider>,
 )
